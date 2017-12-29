00d82cdf6b6c2a434f68cf2941fcdf81fdfddb62
yudielgc <yudielgc@gmail.com>
2017-12-29 17:05:17 -0500

    setting up static conversion

---
+++ site.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  site: {
+    title: 'Best Credit Cards for our customers | Home :: Superior Credit Cards',
+    description: 'Best Credit Cards for our customers, credit card, credit cards, apply for credit card, best credit cards, credit card offers, compare credit cards, top credit cards, credit cards for bad credit, superior credit cards',
+  }
+}
