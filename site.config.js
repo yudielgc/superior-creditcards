8c08bddfb412df647193c1171f709e4ce96fa763
yudielgc <yudielgc@gmail.com>
2017-12-30 14:26:45 -0500

    dynamically reading and rendering categories

--- site.config.js
+++ site.config.js
@@ -1,6 +1,11 @@
+const categories = require('./src/data/categories');
+const creditcards = require('./src/data/credit-cards');
+
 module.exports = {
-  site: {
-    title: 'Best Credit Cards for our customers | Home :: Superior Credit Cards',
-    description: 'Best Credit Cards for our customers, credit card, credit cards, apply for credit card, best credit cards, credit card offers, compare credit cards, top credit cards, credit cards for bad credit, superior credit cards',
-  }
-}
+    site: {
+        title: 'Best Credit Cards for our customers | Home :: Superior Credit Cards',
+        description: 'Best Credit Cards for our customers, credit card, credit cards, apply for credit card, best credit cards, credit card offers, compare credit cards, top credit cards, credit cards for bad credit, superior credit cards',
+        categories,
+        creditcards
+    }
+};
