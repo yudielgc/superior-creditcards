7a8bc75566636eb9dc101ca2b1fc54408a014902
yudielgc <yudielgc@gmail.com>
2017-12-30 16:09:58 -0500

    implementing featured section

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,43 @@
-const fse = require('fs-extra')
-const path = require('path')
-const { promisify } = require('util')
-const ejsRenderFile = promisify(require('ejs').renderFile)
-const globP = promisify(require('glob'))
-const config = require('../site.config')
+const fse = require('fs-extra');
+const path = require('path');
+const { promisify } = require('util');
+const ejsRenderFile = promisify(require('ejs').renderFile);
+const globP = promisify(require('glob'));
+const config = require('../site.config');
 
-const srcPath = './src'
-const distPath = './public'
+const srcPath = './src';
+const distPath = './public';
 
 // clear destination folder
-fse.emptyDirSync(distPath)
+fse.emptyDirSync(distPath);
 
 // copy assets folder
-fse.copy(`${srcPath}/assets`, `${distPath}`)
+fse.copy(`${srcPath}/assets`, `${distPath}`);
+// copy creditcards images
+fse.copy(`${srcPath}/data/images/creditcards`, `${distPath}/images`);
 
 // read page templates
 globP('**/*.ejs', { cwd: `${srcPath}/pages` })
   .then((files) => {
     files.forEach((file) => {
-      const fileData = path.parse(file)
-      const destPath = path.join(distPath, fileData.dir)
+      const fileData = path.parse(file);
+      const destPath = path.join(distPath, fileData.dir);
 
       // create destination directory
       fse.mkdirs(destPath)
         .then(() => {
           // render page
-          return ejsRenderFile(`${srcPath}/pages/${file}`, Object.assign({}, config))
+          return ejsRenderFile(`${srcPath}/pages/${file}`, Object.assign({}, config));
         })
         .then((pageContents) => {
           // render layout with page contents
-          return ejsRenderFile(`${srcPath}/layout.ejs`, Object.assign({}, config, { body: pageContents }))
+          return ejsRenderFile(`${srcPath}/layout.ejs`, Object.assign({}, config, { body: pageContents }));
         })
         .then((layoutContent) => {
           // save the html file
-          fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent)
+          fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent);
         })
-        .catch((err) => { console.error(err) })
+        .catch((err) => { console.error(err) });
     })
   })
-  .catch((err) => { console.error(err) })
+  .catch((err) => { console.error(err) });
--- site.config.js
+++ site.config.js
@@ -6,6 +6,9 @@ module.exports = {
         title: 'Best Credit Cards for our customers | Home :: Superior Credit Cards',
         description: 'Best Credit Cards for our customers, credit card, credit cards, apply for credit card, best credit cards, credit card offers, compare credit cards, top credit cards, credit cards for bad credit, superior credit cards',
         categories,
-        creditcards
+        creditcards,
+        featured: creditcards.filter((creditcard) => {
+            return creditcard.featured;
+        })
     }
 };
